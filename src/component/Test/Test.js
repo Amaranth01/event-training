384cb67e87dae3bee78641552b1cb427d92b4229
Amaranth01 <vanessa.nectoux@gmail.com>
2023-03-28 16:06:29 +0200

    Playing with event

--- src/component/Test/Test.js
+++ src/component/Test/Test.js
@@ -1,42 +1,32 @@
 import './Test.css';
+import {useState} from "react";
 
 export const Test = function () {
-    //Evenement copie : copie colle contenu du 1er input au second
+    //testPaste est pour utiliser la valeur. setter est pour changer la valeur
+    const [testPaste, setTestPaste] = useState('')
+    const [testCopy, setTestCopy] = useState('Hello John');
+
+    //useState renvoie un tableau, mais c'est plus long et décomposé. Autre méthode d'utilisation :
+    // const state = useState('');
+    // const testPase = [0];
+    // const setTestPaste = state[1];
+
     const handleCopy = (e) => {
         const start = e.target.selectionStart;
         const stop = e.target.selectionEnd;
-        e.target.nextElementSibling.value = e.target.value.slice(start, stop)
-    }
-    //Colle ce qu'on met dans le deuxième input dans le textarea
-    const handlePaste = (e) => {
-        e.target.nextElementSibling.value = e.clipboardData.getData('text/plain');
-    }
-
-    //Donne le code de la touche
-    const handleKeyDown = (e) => {
-        console.log(e.keyCode)
-    }
-
-    //au click supprime la cible en question
-    const handleClick = (e) => {
-        e.target.remove()
-    }
-
-    //Ajoute une phrase dans un élément en cas de survol de ce dit élément, même effet avec onMouseEnter
-    const handleEnter = (e) => {
-        e.target.innerText = 'La souris est entrée le chat va la manger'
-    }
-
-    //Désactive le clique droit sur la cible
-    const handleContextMenu = (e) => {
-        e.preventDefault();
+        // setTestPaste( e.target.value.slice(start, stop));
+        setTestPaste( testCopy.slice(start, stop));
+        //testPaste et setTestpaste sont liés maitnenant setTestPaste est utlisé à chaque mise à jour
+        //testCopy dispose d'une valeur par défaut, il n'y a pas besoin d'aller chercher e.target.value. Possibilité d'utiliser directerment la variable du state
     }
 
     return(
       <>
-          <input type="text" name="text-copy" onCopy={handleCopy}/>
-          <input type="text" name='text-paste' onContextMenu={handleContextMenu}/>
-          <textarea name="target" id="target" cols="30" rows="10" contentEditable={false} onMouseOver={handleEnter} />
+          {/*Met à jour l'input si l'utilisateur rentre du texte */}
+          <input type="text" name="text-copy" value={testCopy} onCopy={handleCopy} onChange={e=>setTestCopy(e.target.value)}/>
+          <input type="text" name='text-paste' value={testPaste}/>
+          <div>Valeur de testPaste : <span>{testPaste}</span></div>
       </>
     );
+    //Le state est synchronisé avec le rendu HTML. les valeurs sont mise à jour automatiquement
 }
